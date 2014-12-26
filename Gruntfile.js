8bc3fafa43c7d055c2af05c4e1b2c0a4b657bdca
fooying@qq.com <fooying@qq.com>
2014-12-27 03:02:04 +0800

    Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  // 加载包含 "uglify" 任务的插件。
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // 默认被执行的任务列表。
+  grunt.registerTask('default', ['uglify']);
+
+};
+
+/*HTML去掉注析、换行符 - HtmlMin
+CSS文件压缩合并 – CssMinify
+JS代码风格检查 – JsHint
+JS代码压缩 – Uglyfy
+image压缩 - imagemin*/
---
+++ README.md
@@ -0,0 +1,17 @@
+模块化编程，方便管理。
+自动化编译。
+HTML
+===
+[Jade](http://jade-lang.com/reference/attributes/)
+
+CSS
+===
+[SASS](http://www.w3cplus.com/sassguide/syntax.html)
+
+JavaScript
+===
+[coffescript](http://coffee-script.org/#language)
+
+Grunt
+===
+[Grunt](http://www.gruntjs.net/docs/getting-started/)

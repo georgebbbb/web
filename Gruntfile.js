d9991a627c162d9e3433a9d28c5cca94ff2d608a
fooying@qq.com <fooying@qq.com>
2014-12-27 04:54:32 +0800

    cjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,22 +3,50 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    compass: {
+      dist: {                   // Target
+        options: {              // Target options
+          sassDir: 'src/sass',
+          cssDir: 'deploy/style/css',
+          environment: 'production'
+        },
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+    },
+    jade: {
+      dist: {
+        options: {
+          data: {
+            debug: false
+          }
+        },
+        files: {
+          "deploy/index.html": "src/jade/index.jade"
+        },
       }
-    }
+    },
+    coffee: {
+      dist: {
+        expand: true,
+        flatten: true,
+        cwd: 'src/coffeescript',
+        src: ['*.coffee'],
+        dest: 'deploy/script/js',
+        ext: '.js'
+      }
+    },
   });
 
   // 加载包含 "uglify" 任务的插件。
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-jade');
 
   // 默认被执行的任务列表。
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', [
+    'compass',
+    'jade',
+    'coffee'
+  ]);
 
 };
 
